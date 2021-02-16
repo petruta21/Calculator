19ab7ef073356691ce950c7b415dcb4a598ebff4
petruta21 <tatsiana.petrushenka@googlemail.com>
2021-02-16 11:22:41 +0300

    more more commit

--- src/com/example/idea/Calculator.java
+++ src/com/example/idea/Calculator.java
@@ -1,11 +1,11 @@
 package com.example.idea;
 
-import static com.example.java.util.m
+
 
 public class Calculator {
 
     public static void main(String[] args) {
-         String s1 = getInput (prmpt."Enter a number value: ");
+         String string1 = getInput ("Enter a number value: ");
     }
 
 
